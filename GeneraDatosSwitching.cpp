e2335e921551d985fd81b23741277eabdadfe6b1
Maxi Antonelli <Maxi Antonelli>
2016-02-15 21:01:47 -0300

    Ahora no anda free(PDFhist) la segunda vez que pasa opr ahí

--- GeneraDatosSwitching.cpp
+++ GeneraDatosSwitching.cpp
@@ -6,12 +6,14 @@
 
 int main()
 {
-    unsigned long int NIter = 2;//100; // Es el largo de cada atractor
-    unsigned int NInitialConditions = 10; // Es la cantidad de condiciones iniciales diferentes de los que se larga el atractor.
+    unsigned long int NIter = 100; // Es el largo de cada atractor
+    unsigned int NInitialConditions = 2; // Es la cantidad de condiciones iniciales diferentes de los que se larga el atractor.
     double Bases[] = {2, 10}; // Vector con las bases que quiero probar
-    double Precisions[] = {1, 2};//, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+    double Precisions[] = {1, 2}; //, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30}; //Contiene todas las precisiones que voy barriendo
     unsigned long int Bins = 10; // Cantidad de bines del histograma
     unsigned long int DimEmb = 3; // Dimensión de embedding para MP, BP y BPW
+
+    double InitialConditions[NInitialConditions]; // Declaro el vector de condiciones iniciales
 
     double* Map; //Declare the pointer
     Map = (double*) malloc (sizeof(double)*NIter+1); //Creates the array. It has one more postition at first for the length
@@ -26,51 +28,87 @@ int main()
 
     double Hhist, Qhist, Chist, Hbp, Qbp, Cbp, Hbpw, Qbpw, Cbpw, MP, Period; // Vectores en donde van guardados los cuantificadores
 
-
-        for (int iBases = 0; iBases <  NBases; iBases++) // Va recorriendo el vector de bases
-        {
-            sprintf(StrAux,"LogB%.0f.dat", Bases[iBases]); //Arma el nombre del archivo para esta base
-            FILE* Results=fopen(StrAux,"w"); // Abre archivo de resultados
-
-    for (int iInitialCondition = 0; iInitialCondition < NInitialConditions; iInitialCondition++) // Va sorteando condiciones iniciales
+    for (int iInitialCondition = 0; iInitialCondition < NInitialConditions; iInitialCondition++) // Lleno un vector de condiciones iniciales
     {
-        double InitialCondition = (double)rand()/(double)RAND_MAX; // Sortea el primer valor del mapa, es una variable uniformemente distribuída entre 0 y 1
+        InitialConditions[iInitialCondition] = (double)rand()/(double)RAND_MAX; // Sortea el primer valor del mapa, es una variable uniformemente distribuída entre 0 y 1
+    }
+    printf("Generated %lu initial conditions\n", NInitialConditions);
 
-            for (int iPrecisions = 0; iPrecisions < NPrecisions; iPrecisions++) //Recorro el vector de precisiones
+    for (int iBases = 0; iBases <  NBases; iBases++) // Va recorriendo el vector de bases
+    {
+        printf("\tBase = %d/%d\n", (int)iBases+1, (int)NBases); // Para debuguear
+
+        sprintf(StrAux,"LogB%.0f.dat", Bases[iBases]); //Arma el nombre del archivo para esta base
+        FILE* Results=fopen(StrAux,"w"); // Abre archivo de resultados
+
+        for (int iPrecisions = 0; iPrecisions < NPrecisions; iPrecisions++) //Recorro el vector de precisiones
+        {
+            printf("\t\tPrecision = %d/%d\n", (int)iPrecisions+1, (int)NPrecisions); // Para debuguear
+
+            Scale = pow(Bases[iBases],Precisions[iPrecisions]); // calculo el valor de la escala para redondear. pow sirve para double
+
+            Hhist = 0;
+            Qhist = 0;
+            Chist = 0;
+            Hbp = 0;
+            Qbp = 0;
+            Cbp = 0;
+            Hbpw = 0;
+            Qbpw = 0;
+            Cbpw = 0;
+            MP = 0;
+            Period = 0;
+
+            for (unsigned long int iInitialCondition = 0; iInitialCondition < NInitialConditions; iInitialCondition++) // Va sorteando condiciones iniciales
             {
-                Scale = pow(Bases[iBases],Precisions[iPrecisions]); // powl sirve para long double
-                Map[1] = floor(Scale*InitialCondition)/Scale; // floorl sirve para long double, como son mapas positivos puedo usar floor en vez de round
+                printf("\t\t\tCondicion inicial = %d/%d\n", (int)iInitialCondition+1, (int)NInitialConditions); // Para debuguear
+
+                Map[1] = floor(Scale*InitialConditions[iInitialCondition])/Scale; // floorl sirve para long double, como son mapas positivos puedo usar floor en vez de round
 
                 for (unsigned long int iMap = 1; iMap < NIter; iMap++) // Va riterando el mapa logístico
                 {
                     Map[iMap+1] =  4*InvScale*floorl(Scale*Map[iMap]*(Map[iMap]-1)); // Mapa logístico, x[n] = r*x[n-1]*(1-x[n-1]), caótico con r=4
                 } // Acá ya tengo el atractor guardado en el vector Map
 
+
                 //Period = find_period(Map); //Tengo que hacer esta función!!!!!!!!!!!
-
-                double* PDFhist=PDF_hist(Map,Bins); // Genera el histograma de patrones de órden
-                Hhist=entropy(PDFhist); // Le calcula la entropía
-                Qhist=disequilibrum(PDFhist); // Le calcula el desequilibrio
-                Chist=Hhist*Qhist; // Le calcula la complejidad
+printf("\t\ta\n"); // Para debuguear
+                double* PDFhist = PDF_hist(Map,Bins); // Genera el histograma de patrones de órden
+printf("\t\tb\n"); // Para debuguear
+                Hhist = Hhist + entropy(PDFhist); // Le calcula la entropía y la suma para el promedio
+printf("\t\tc\n"); // Para debuguear
+                Qhist = Qhist + disequilibrum(PDFhist); // Le calcula el desequilibrio
+printf("\t\td\n"); // Para debuguear
+                Chist = Chist + Hhist*Qhist; // Le calcula la complejidad
+printf("\t\te\n"); // Para debuguear
                 free(PDFhist); // Libera el vector que contiene al histograma
+printf("\t\tf\n"); // Para debuguear
 
-                double* PDFbp=PDF_BP_CS(Map,DimEmb); // Genera el histograma de patrones de órden
-                Hbp=entropy(PDFbp); // Le calcula la entropía
-                Qbp=disequilibrum(PDFbp); // Le calcula el desequilibrio
-                Cbp=Hbp*Qbp; // Le calcula la complejidad
+                double* PDFbp = PDF_BP_CS(Map,DimEmb); // Genera el histograma de patrones de órden
+                Hbp = Hbp + entropy(PDFbp); // Le calcula la entropía
+                Qbp = Qbp + disequilibrum(PDFbp); // Le calcula el desequilibrio
+                Cbp = Cbp + Hbp*Qbp; // Le calcula la complejidad
                 free(PDFbp); // Libera el vector que contiene al histograma
 
-                double* PDFbpw=PDF_BPW(Map,DimEmb); // Genera el histograma de patrones de órden
-                Hbpw=entropy(PDFbpw); // Le calcula la entropía
-                Qbpw=disequilibrum(PDFbpw); // Le calcula el desequilibrio
-                Cbpw=Hbpw*Qbpw; // Le calcula la complejidad
+                double* PDFbpw = PDF_BPW(Map,DimEmb); // Genera el histograma de patrones de órden
+                Hbpw = Hbpw + entropy(PDFbpw); // Le calcula la entropía
+                Qbpw = Qbpw + disequilibrum(PDFbpw); // Le calcula el desequilibrio
+                Cbpw = Cbpw + Hbpw*Qbpw; // Le calcula la complejidad
                 free(PDFbpw); // Libera el vector que contiene al histograma
-
-
-        }
-    }
-                //Acá tengo que hacer un promedio para todas las condiciones iniciales sorteadas, los cuantificadores de arriba tengo que guardarlos en un vactor
-                fprintf(Results,"%.0f\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%d%d\n", Precisions[iPrecisions], Hhist, Qhist, Chist, Hbp, Qbp, Cbp, Hbpw, Qbpw, Cbpw, (unsigned long int)MP, (unsigned long int)Period); //Guarda los valores en el archivo de salida, escribo la condición inicial para evaluar el comportamiento del rand()
             }
-            fclose(Results); // Cierra el archivo de salida, queda un archivo por base para este oscilador
+            Hhist = Hhist/(double)NInitialConditions;
+            Qhist = Qhist/(double)NInitialConditions;
+            Chist = Chist/(double)NInitialConditions;
+            Hbp = Hbp/(double)NInitialConditions;
+            Qbp = Qbp/(double)NInitialConditions;
+            Cbp = Cbp/(double)NInitialConditions;
+            Hbpw = Hbpw/(double)NInitialConditions;
+            Qbpw = Qbpw/(double)NInitialConditions;
+            Cbpw = Cbpw/(double)NInitialConditions;
+            MP = MP/(double)NInitialConditions;
+            Period = Period/(double)NInitialConditions;
+            fprintf(Results,"%.0f\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%d\t%d\n", Precisions[iPrecisions], Hhist, Qhist, Chist, Hbp, Qbp, Cbp, Hbpw, Qbpw, Cbpw, (unsigned long int)MP, (unsigned long int)Period); //Guarda los valores en el archivo de salida, escribo la condición inicial para evaluar el comportamiento del rand()
+        }
+        fclose(Results); // Cierra el archivo de salida, queda un archivo por base para este oscilador
+    }
 }
